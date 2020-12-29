375ee043b0c1070451e9aa4d1416c97562311613
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2020-12-29 15:41:11 +0530

    Initial Vue 3 App With Router & Vuex

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+import { createApp } from "vue"
+import App from "./App"
+import router from "./router"
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
